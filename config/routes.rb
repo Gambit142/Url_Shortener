f37bc0d0d783f666acab72ca93b8a6882f01450c
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-12-01 15:26:49 +0100

    Create routes for models created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :long_urls, only: [:new, :create, :show] do
+    resources :short_urls, only: [:new, :create]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
