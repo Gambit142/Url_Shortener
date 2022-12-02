6ba1210ec7fd787029e0d28a408b32a33c3e2e68
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-12-02 15:44:41 +0100

    Create routes needed for project implementation

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
     resources :short_urls, only: [:create]
   end
 
+  get '/:short_url', to: 'short_urls#show', as: :short_url
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "long_urls#show"
 end
