86ef746bac2e819cc332d4c07d1a5a2fd0f20032
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-12-05 23:22:31 +0100

    Ad validations for short_url model attributes

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,4 +1,7 @@
 class ShortUrl < ApplicationRecord
   belongs_to :long_url
   belongs_to :user
+
+  validates :url, presence: true, uniqueness: true
+  validates :alias, length: { maximum: 16 }, uniqueness: true, allow_blank: true
 end
