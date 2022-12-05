0371ab779ebf7a4df9be7877ad99b5dd8cfc14c9
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-12-05 23:19:43 +0100

    Configure additional settings for Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || long_urls_path
+  end
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation)}
+  end
 end
