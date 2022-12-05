1e3461a7833b1f66ace62d032164d2ea5516f71a
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-12-05 23:21:32 +0100

    Write methds for shortening url and redirecting them

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -1,27 +1,48 @@
 class ShortUrlsController < ApplicationController
+  before_action :get_url, only: [:show]
+
+  def show
+  end
+
   def create
-    custom_name = short_url_params[:alias]
-    @long_url = LongUrl.create!(url: short_url_params[:url])
-    if custom_name.present?
-      @short_url = ShortUrl.create!(long_url: @long_url, url: custom_name)
+    shortened_url = create_short_url
+    @long_url = LongUrl.create!(url: short_url_params[:long_url])
+    @short_url = current_user.short_urls.new(long_url: @long_url, url: shortened_url)
+    if @short_url.save
+      redirect_to @short_url
+      flash[:success] = "Short URL created successfully"
     else
-      hashed_string = Digest::SHA1.hexdigest(@long_url&.url)[0..5]
-      @short_url = ShortUrl.create!(long_url: @long_url, url: hashed_string)
+      redirect_to long_urls_path
+      flash[:notice] = "Could not create short URL"
     end
   end
 
   def redirect_url
-    @short_url = ShortUrl.find_by(url: params[:short_code])
+    @short_url = ShortUrl.find_by(url: params[:short_url])
     if @short_url
-      redirect_to @short_url.long_url.url
+      redirect_to @short_url.long_url.url, allow_other_host: true # pass the url to the browser
     else
-      render :not_found
+      render file: "#{Rails.root}/public/404.html", status: 404
     end
   end
 
   private
 
   def short_url_params
-    params.require(:short_url).permit(:url, :alias).merge(long_url: :url)
+    params.require(:short_url).permit(:url, :alias).merge(long_url: params[:url])
+  end
+
+  def create_short_url
+    custom_name = short_url_params[:alias]
+    if custom_name.present?
+      @short_url = custom_name
+    else
+      hashed_string = Digest::SHA1.hexdigest(short_url_params[:long_url])[0..5]
+      @short_url = hashed_string
+    end
+  end
+
+  def get_url
+    @short_url = ShortUrl.find(params[:id])
   end
 end
