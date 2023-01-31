f18839cb5b686309653fe29a3b0eacfb05df8bde
Gambit142 <nzefrancismaryeugorji@gmail.com>
2023-01-31 23:10:39 +0100

    Test output for Ruby methods

---
+++ test.rb
@@ -0,0 +1,27 @@
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(age, name: 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+end
+
+person = Person.new(18, name: 'John', parent_permission: true)
+
+puts person.of_age?# true
